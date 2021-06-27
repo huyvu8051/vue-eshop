b010220b077d2c3b695b53d287eb333d20267535
Huy Vu Van <huyvu8051@gmail.com>
2021-06-27 10:15:25 +0700

    create login and set loading = true before install axios package

---
+++ eshop/src/components/pages/Login.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="login">
+      <div class="loginHeader">
+          User Login
+      </div>
+      <div class="loginContainer">
+          <table>
+              <tr>
+                  <td>
+                      UserName
+                  </td>
+                  <td>:</td>
+                  <td>
+                      <input type="text" name="username" id="username" placeholder="Username"
+                        v-model="user.username">
+                  </td>
+              </tr>
+              <tr>
+                  <td>
+                      UserName
+                  </td>
+                  <td>:</td>
+                  <td>
+                      <input type="password" name="password" id="password" placeholder="Password"
+                        v-model="user.password">
+                  </td>
+              </tr>
+              <tr>
+                  <td>
+                  </td>
+                  <td></td>
+                  <td>
+                      <button type="submit" class="addBtn" @click="login">Login</button>
+                  </td>
+              </tr>
+          </table>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      msg: '',
+      user: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    login () {
+      console.log(this.user)
+      this.$eventBus.$emit('loadingStatus', true)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
