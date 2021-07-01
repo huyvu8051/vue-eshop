ec16ba1437d3a6c629b8c5c203955b8d07e74864
Huy Vu Van <huyvu8051@gmail.com>
2021-07-01 21:42:08 +0700

    public client to ip

--- eshop/src/services/Api.js
+++ eshop/src/services/Api.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 import store from '@/store/store'
 
+// `http://localhost:8081/`
+
 export default () => {
   return axios.create({
     baseURL: `http://localhost:8081/`,
