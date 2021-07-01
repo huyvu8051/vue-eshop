52b58d06d477d5c857a418f4f376b7c888392148
Huy Vu Van <huyvu8051@gmail.com>
2021-07-01 22:46:50 +0700

    deploy server into open ip address to make another device can access the website

--- eshop/src/services/Api.js
+++ eshop/src/services/Api.js
@@ -1,11 +1,17 @@
 import axios from 'axios'
 import store from '@/store/store'
+const ipRegex = require('ip-regex')
+
+var ipAddress = window.location.host
+var match = ipAddress.match(ipRegex())
+
+var baseUrl = window.location.protocol + '//' + match[0] + ':8081/'
 
 // `http://localhost:8081/`
 
 export default () => {
   return axios.create({
-    baseURL: `http://localhost:8081/`,
+    baseURL: baseUrl,
     headers: {
       token: `${store.state.token}`,
       signature: `${store.state.signature}`
